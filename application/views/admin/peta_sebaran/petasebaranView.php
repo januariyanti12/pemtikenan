67fd7a3cdc525f3aeff303d7dbcd87ab21eeda42
januariyanti12 <januariyanti60@gmail.com>
2022-01-15 00:43:22 -0800

    peta

--- application/views/admin/peta_sebaran/petasebaranView.php
+++ application/views/admin/peta_sebaran/petasebaranView.php
@@ -12,12 +12,13 @@
             }).addTo(map);
 
             <?php
-            $connections = mysqli_connect("localhost", "root", "", "datakeluargasejahtera");
+            // $connections = mysqli_connect("localhost", "root", "", "datakeluargasejahtera");
 
-            $sql = "select * from penduduk p inner join jenis_kb b on p.id_jenis_kb=b.id_jenis_kb inner join jenis_rt c on p.id_jenis_rt=c.id_jenis_rt inner join jenis_ks d on p.id_jenis_ks=d.id_jenis_ks inner join lokasi l on p.id_lokasi=l.id_lokasi";
-            $data = mysqli_query($connections, $sql);
+            // $sql = "select * from penduduk p inner join jenis_kb b on p.id_jenis_kb=b.id_jenis_kb inner join jenis_rt c on p.id_jenis_rt=c.id_jenis_rt inner join jenis_ks d on p.id_jenis_ks=d.id_jenis_ks inner join lokasi l on p.id_lokasi=l.id_lokasi";
+            // $data = mysqli_query($connections, $sql);
             $js = '';
-            while ($row = mysqli_fetch_assoc($data)) {
+            // while ($row = mysqli_fetch_assoc($data)) {
+            foreach ($peta as $row) {
                 $js .= 'L.marker([
 						' . $row['lat'] . ',
 						' . $row['lng'] . '
